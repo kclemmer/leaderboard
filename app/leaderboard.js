1c200db0ad8ff50c271a0a5bc2bce63244e8b091
Kenji Clemmer <clemmer@hawaii.edu>
2017-02-28 11:45:37 -1000

    What I did So far...

--- app/leaderboard.js
+++ app/leaderboard.js
@@ -1,1 +1,22 @@
 console.log("Hello world");
+
+PlayersList = new Mongo.Collection('players');
+
+if(Meteor.isClient){
+  // this code only runs on the client
+  Template.leaderboard.helpers({
+    'player': function(){
+      return PlayersList.find();
+    },
+  });
+
+  Template.leaderboard.events({
+    'click .player': function(){
+      console.log("You clicked a .player element");
+    }
+  });
+}
+
+if(Meteor.isServer){
+  // this code only runs on the server
+}
